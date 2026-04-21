cfb025459859bd641fb88955b4be51b3e440d728
khanamAreeba <khanamareeba7@gmail.com>
2026-04-21 02:19:32 -0400

    commit

--- node-project/routes/patients.route.js
+++ node-project/routes/patients.route.js
@@ -18,11 +18,11 @@ import { auth } from "../middlewares/auth.middleware.js";
 router.get("/export", auth, roleMiddleware("Admin"), exportPatientsCSV);
 router.get("/getPatientByAppointmentId/:appointmentId", auth, roleMiddleware("Admin", "Doctor", "FDO"), getPatientByAppointmentId);
 
-router.post("/", auth, roleMiddleware("FDO","Admin"), createPatient);
+router.post("/", auth, roleMiddleware("FDO", "Admin"), createPatient);
 router.get("/", auth, roleMiddleware("Admin", "FDO"), getAllPatients);
 
 router.get("/:id", auth, roleMiddleware("FDO"), getPatientById);
-router.put("/:id", auth, roleMiddleware("FDO","Admin"), updatePatient);
+router.put("/:id", auth, roleMiddleware("FDO", "Admin"), updatePatient);
 router.delete("/:id", auth, roleMiddleware("Admin"), deletePatient);
 router.get("/appointmentCount/total", getTotalAppointmentCount);
 
